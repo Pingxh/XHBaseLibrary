96afb6a9f8c457ee2cf097ff078790ef63319410
pingxh <pingxhcn@163.com>
2020-03-19 19:13:45 +0800

    更新 .h 文件

--- Classes/XHBaseLibrary.h
+++ Classes/XHBaseLibrary.h
@@ -6,23 +6,20 @@
 //  Copyright © 2020 Yioks. All rights reserved.
 //
 
+#import "UIFont+Config.h"
+#import "NSDate+Category.h"
 #import "NSObject+Property.h"
-
+#import "NSString+Category.h"
+#import "UIAlertController+Extend.h"
+#import "UIButton+CountDown.h"
 #import "UIColor+Hex.h"
 #import "UIDevice+Info.h"
-#import "UIButton+CountDown.h"
-#import "UIView+Extension.h"
-#import "UIView+Category.h"
 #import "UIImage+Category.h"
 #import "UIImage+Rotate.h"
 #import "UIResponder+Category.h"
-#import "UIAlertController+Extend.h"
-#import "UIFont+Config.h"
-#import "NSString+Category.h"
+#import "UIView+Extension.h"
+#import "UIView+Category.h"
 
 #import "UserInfoSaveManager.h"
 #import "Localizable.h"
-
-#import "UITableView+NoData.h"
-#import "UICollectionView+NoData.h"
-#import "NSDate+Category.h"
+#import "NSBundle+Language.h"
