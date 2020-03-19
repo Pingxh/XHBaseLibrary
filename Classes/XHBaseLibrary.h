b21a4ecfd803ee0cb11fd5b1e6aa42b6a722c807
pingxh <pingxhcn@163.com>
2020-03-19 18:50:44 +0800

    添加 .h 文件

---
+++ Classes/XHBaseLibrary.h
@@ -0,0 +1,29 @@
+//
+//  XHBaseLibrary.h
+//  XHBaseLibrary
+//
+//  Created by Apple on 2020/3/19.
+//  Copyright © 2020 Yioks. All rights reserved.
+//
+
+#import "NSObject+Property.h"
+
+#import "UIColor+Hex.h"
+#import "UIDevice+Info.h"
+#import "UIButton+CountDown.h"
+#import "UIView+Extension.h"
+#import "UIView+Category.h"
+#import "UIImage+Category.h"
+#import "UIImage+Rotate.h"
+#import "UIResponder+Category.h"
+#import "UIAlertController+Extend.h"
+#import "UIFont+Config.h"
+#import "NSString+Category.h"
+
+#import "UserInfoSaveManager.h"
+#import "DeviceManager.h"
+#import "Localizable.h"
+
+#import "UITableView+NoData.h"
+#import "UICollectionView+NoData.h"
+#import "NSDate+Category.h"
--- XHBaseLibrary.podspec
+++ XHBaseLibrary.podspec
@@ -16,7 +16,7 @@ Pod::Spec.new do |spec|
   #
 
   spec.name         = "XHBaseLibrary"
-  spec.version      = "0.0.6"
+  spec.version      = "0.0.7"
   spec.summary      = "XHBaseLibrary 基础类封装"
 
   # This description is used to generate tags and improve search results.
